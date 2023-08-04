322c7197ba50432c514c75c56b00c9d733b39e8a
Huseynov Yunis <yunisah@code.local>
2023-08-04 17:41:36 +0400

    html css done

---
+++ Tabs/script.js
@@ -0,0 +1,24 @@
+let myData = [
+    {
+        id: 1, name: "Codepen", text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae autem
+    aliquid accusantium eaque, necessitatibus repellendus voluptas quae
+    corrupti officiis, quam provident omnis quis fugiat doloremque. Ipsa
+    quis error vel eos, porro voluptatibus eaque et quidem atque neque nam
+    nemo assumenda exercitationem harum velit numquam soluta tenetur,
+    omnis, sunt nulla facilis!`},
+    {
+        id: 2, name: "Dribble", text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+        obcaecati neque placeat esse blanditiis labore atque molestias
+        doloremque natus commodi?`},
+    {
+        id: 3, name: "Dropbox", text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam odio
+        cupiditate fuga. Impedit, tempore beatae? Expedita reprehenderit optio
+        quisquam consequatur sint quis iure laudantium quia corrupti nobis
+        aperiam voluptate qui, nesciunt dolorum error deserunt quos, nostrum
+        eveniet atque, distinctio odio.`},
+    {
+        id: 4, name: "Dropbox", text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+        totam nesciunt doloribus deleniti assumenda ad quam porro voluptatum
+        eius necessitatibus!`}
+
+]
