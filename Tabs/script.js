c627e2e8ecf7cdd9e8bd00f3ef83c96b4e9182b8
fiendgrin <yunisah@code.edu.az>
2023-08-05 21:17:04 +0400

    Tabs done

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,24 +1,71 @@
 let myData = [
-    {
-        id: 1, name: "Codepen", text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae autem
-    aliquid accusantium eaque, necessitatibus repellendus voluptas quae
-    corrupti officiis, quam provident omnis quis fugiat doloremque. Ipsa
-    quis error vel eos, porro voluptatibus eaque et quidem atque neque nam
-    nemo assumenda exercitationem harum velit numquam soluta tenetur,
-    omnis, sunt nulla facilis!`},
-    {
-        id: 2, name: "Dribble", text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-        obcaecati neque placeat esse blanditiis labore atque molestias
-        doloremque natus commodi?`},
-    {
-        id: 3, name: "Dropbox", text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam odio
-        cupiditate fuga. Impedit, tempore beatae? Expedita reprehenderit optio
-        quisquam consequatur sint quis iure laudantium quia corrupti nobis
-        aperiam voluptate qui, nesciunt dolorum error deserunt quos, nostrum
-        eveniet atque, distinctio odio.`},
-    {
-        id: 4, name: "Dropbox", text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-        totam nesciunt doloribus deleniti assumenda ad quam porro voluptatum
-        eius necessitatibus!`}
+  {
+    id: 1,
+    name: "Codepen",
+    icon: "codepen.svg",
+    text: `Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
 
-]
+    Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.`,
+  },
+  {
+    id: 2,
+    name: "Dribble",
+    icon: "dribble.svg",
+    text: `Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+
+    Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+    Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+
+    Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.`,
+  },
+  {
+    id: 3,
+    name: "Dropbox",
+    icon: "dropbox.svg",
+    text: `Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+
+    Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.`,
+  },
+  {
+    id: 4,
+    name: "Drupal",
+    icon: "drupal.svg",
+    text: `Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+
+    Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.`,
+  },
+];
+
+function tabMaker(id, name, icon) {
+  return `<div id="${id}" class="tab">
+  <img src="./icons/${icon}" alt="" />${name}</div>`;
+}
+
+function textFiller(id, text) {
+  return `<div id="${id}" class="pText">${text}</div>`;
+}
+
+const tabs = document.querySelector(".tabs");
+const bottomText = document.querySelector(".text");
+
+for (const tab of myData) {
+  tabs.innerHTML += tabMaker(tab.id, tab.name, tab.icon);
+}
+
+tabs.firstElementChild.classList.add("active");
+bottomText.innerHTML = textFiller(myData[0].id, myData[0].text);
+const tabButtons = document.querySelectorAll(".tab");
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    tabButtons.forEach((btnActive) => {
+      btnActive.classList.remove("active");
+    });
+    myData.forEach((data) => {
+      if (button.id == data.id) {
+        button.classList.add("active");
+        bottomText.innerHTML = textFiller(data.id, data.text);
+      }
+    });
+  });
+});
